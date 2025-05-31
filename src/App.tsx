aeb120711c9b53b5d1c229f6ef8a73f19c135648
FlowOFF <nettoaeb1@gmail.com>
2025-05-31 07:43:25 -0300

    chore: adiciona .gitignore padrão para projeto React/Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Reader from './components/Reader';
@@ -8,28 +7,26 @@ import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import { ChatbaseBubble } from './components/ChatbaseBubble';
 import WebTalk from './pages/WebTalk';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
   const location = useLocation();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <div className="min-h-screen bg-gradient-to-b from-zinc-50 to-zinc-100 font-montserrat">
-            <Header />
-            <Hero />
-            <Reader />
-            <Headline />
-            <Benefits />
-            <FAQ />
-            <Footer />
-            {location.pathname !== "/webtalk" && <ChatbaseBubble />}
-          </div>
-        } />
-        <Route path="/webtalk" element={<WebTalk />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={
+        <div className="min-h-screen bg-gradient-to-b from-zinc-50 to-zinc-100 font-montserrat">
+          <Header />
+          <Hero />
+          <Reader />
+          <Headline />
+          <Benefits />
+          <FAQ />
+          <Footer />
+          {location.pathname !== "/webtalk" && <ChatbaseBubble />}
+        </div>
+      } />
+      <Route path="/webtalk" element={<WebTalk />} />
+    </Routes>
   );
 }
 
